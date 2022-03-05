53522530f894ddb30e0ebe72a346ea6cc00da296
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-03-04 20:22:44 -0800

    Run gdformat.

--- addons/seurat_capture/capture_gizmo.gd
+++ addons/seurat_capture/capture_gizmo.gd
@@ -5,49 +5,52 @@ extends EditorSpatialGizmoPlugin
 
 const CaptureSpatial = preload("res://addons/seurat_capture/capture_spatial.gd")
 
+
 func get_name():
 	return "SeuratCaptureBox"
 
+
 func has_gizmo(spatial):
 	return spatial is CaptureSpatial
-	
-	
+
+
 func redraw(gizmo):
 	gizmo.clear()
-	
+
 	var spatial = gizmo.get_spatial_node()
-	
+
 	var lines = PoolVector3Array()
 
 	# the wireframe box of the capture volume
-	lines.push_back(Vector3(-1, -1, -1)) #front
-	lines.push_back(Vector3( 1, -1, -1))
-	lines.push_back(Vector3( 1, -1, -1))
-	lines.push_back(Vector3( 1,  1, -1))
-	lines.push_back(Vector3( 1,  1, -1))
-	lines.push_back(Vector3(-1,  1, -1))
-	lines.push_back(Vector3(-1,  1, -1))
+	lines.push_back(Vector3(-1, -1, -1))  #front
+	lines.push_back(Vector3(1, -1, -1))
+	lines.push_back(Vector3(1, -1, -1))
+	lines.push_back(Vector3(1, 1, -1))
+	lines.push_back(Vector3(1, 1, -1))
+	lines.push_back(Vector3(-1, 1, -1))
+	lines.push_back(Vector3(-1, 1, -1))
 	lines.push_back(Vector3(-1, -1, -1))
 
-	lines.push_back(Vector3(-1, -1,  1)) #back
-	lines.push_back(Vector3( 1, -1,  1))
-	lines.push_back(Vector3( 1, -1,  1))
-	lines.push_back(Vector3( 1,  1,  1))
-	lines.push_back(Vector3( 1,  1,  1))
-	lines.push_back(Vector3(-1,  1,  1))
-	lines.push_back(Vector3(-1,  1,  1))
-	lines.push_back(Vector3(-1, -1,  1))
-	
-	lines.push_back(Vector3(-1, -1, -1)) #side lines
-	lines.push_back(Vector3(-1, -1,  1))
-	lines.push_back(Vector3( 1, -1, -1))
-	lines.push_back(Vector3( 1, -1,  1))
-	lines.push_back(Vector3( 1,  1, -1))
-	lines.push_back(Vector3( 1,  1,  1))
-	lines.push_back(Vector3(-1,  1, -1))
-	lines.push_back(Vector3(-1,  1,  1))
-	
+	lines.push_back(Vector3(-1, -1, 1))  #back
+	lines.push_back(Vector3(1, -1, 1))
+	lines.push_back(Vector3(1, -1, 1))
+	lines.push_back(Vector3(1, 1, 1))
+	lines.push_back(Vector3(1, 1, 1))
+	lines.push_back(Vector3(-1, 1, 1))
+	lines.push_back(Vector3(-1, 1, 1))
+	lines.push_back(Vector3(-1, -1, 1))
+
+	lines.push_back(Vector3(-1, -1, -1))  #side lines
+	lines.push_back(Vector3(-1, -1, 1))
+	lines.push_back(Vector3(1, -1, -1))
+	lines.push_back(Vector3(1, -1, 1))
+	lines.push_back(Vector3(1, 1, -1))
+	lines.push_back(Vector3(1, 1, 1))
+	lines.push_back(Vector3(-1, 1, -1))
+	lines.push_back(Vector3(-1, 1, 1))
+
 	gizmo.add_lines(lines, get_material("main", gizmo), false)
+
 
 func _init():
 	create_material("main", Color(1, 0, 0))
--- addons/seurat_capture/seurat_capture.gd
+++ addons/seurat_capture/seurat_capture.gd
@@ -1,19 +1,18 @@
-# Seurat Capture Plugin
 tool
 extends EditorPlugin
 
 const SeuratCaptureBox = preload("res://addons/seurat_capture/capture_gizmo.gd")
 
-var capture_box = SeuratCaptureBox.new();
+var capture_box = SeuratCaptureBox.new()
+
 
 func _enter_tree():
-	add_custom_type("SeuratCaptureBox", "Spatial", SeuratCaptureBox.CaptureSpatial, preload("spatial_icon.png"))
-	add_spatial_gizmo_plugin(capture_box);
-	pass
+	add_custom_type(
+		"SeuratCaptureBox", "Spatial", SeuratCaptureBox.CaptureSpatial, preload("spatial_icon.png")
+	)
+	add_spatial_gizmo_plugin(capture_box)
 
 
 func _exit_tree():
-	remove_spatial_gizmo_plugin(capture_box);
-	remove_custom_type("SeuratCaptureBox");
-	pass;
-
+	remove_spatial_gizmo_plugin(capture_box)
+	remove_custom_type("SeuratCaptureBox")
